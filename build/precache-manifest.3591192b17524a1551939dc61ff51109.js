c5e26210afdba21751946a43819fb0f581f2f315
eron c <deandradeeron@gmail.com>
2019-07-25 13:52:31 -0300

    Npm run build

---
+++ build/precache-manifest.3591192b17524a1551939dc61ff51109.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019836c31bc8e39877904f804dee77c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f1081e0e3e87decca4",
+    "url": "/static/css/main.de7fea32.chunk.css"
+  },
+  {
+    "revision": "6af0dd5885414f1becf5",
+    "url": "/static/js/2.37e2b95e.chunk.js"
+  },
+  {
+    "revision": "60f1081e0e3e87decca4",
+    "url": "/static/js/main.bb08f335.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d88e644b8c8548a715084de6c4066a67",
+    "url": "/static/media/46952635055_6c83f11ee4_c.d88e644b.jpg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
